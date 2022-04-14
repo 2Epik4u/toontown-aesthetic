85c494d1b2ac9b8109733bc3918b4de4d980a9bd
Thunderzapper <justinabossmlg@gmail.com>
2022-04-14 01:20:28 -0400

    stuff 4 building

---
+++ build.bat
@@ -0,0 +1,4 @@
+@echo off
+multify.exe -c -f aesthetic.mf phase_3 phase_3.5 phase_4 phase_5 phase_5.5 phase_6 phase_7 phase_8 phase_9 phase_10 phase_11 phase_12 phase_13 info.ini pack.png
+echo it done i think
+pause
