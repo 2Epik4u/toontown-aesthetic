f39d18c408233fd7ceb7a215aa0f562293d37812
Thunderzapper <justinabossmlg@gmail.com>
2022-04-14 01:44:11 -0400

    bruh

--- build.bat
+++ build.bat
@@ -1,4 +1,5 @@
 @echo off
+git pull
 multify.exe -c -f aesthetic.mf phase_3 phase_3.5 phase_4 phase_5 phase_5.5 phase_6 phase_7 phase_8 phase_9 phase_10 phase_11 phase_12 phase_13 info.ini pack.png
 echo it done i think
 pause
